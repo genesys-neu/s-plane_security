70029aa04fd5366bc0843bfce1d1aba25183c2af
jgroen <josh.groen@gmail.com>
2024-09-24 16:11:00 -0400

    testing a different method

--- pipeline_demo2.py
+++ pipeline_demo2.py
@@ -204,7 +204,7 @@ def acquisition_from_file(packet_queue, file_path, initial_time):
                         except Scapy_Exception as e:
                             print(f'Exception {e}, waiting for more data')
                             break  # Break the inner loop if an exception occurs
-        print(f'Took {1000*(time.time()-start)} ms to complete try loop')
+            print(f'Took {1000*(time.time()-start)} ms to complete try loop')
 
         except Exception as e:
             print(f"Error reading from file: {str(e)}")
