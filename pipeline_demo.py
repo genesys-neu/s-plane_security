c5f00b4665ea09279061d33265ed77dbe158f7c6
jgroen <josh.groen@gmail.com>
2024-09-24 12:30:27 -0400

    troubleshooting

--- pipeline_demo.py
+++ pipeline_demo.py
@@ -148,10 +148,11 @@ def acquisition_from_file(packet_queue, file_path, initial_time):
                                 ptp_info.append(int.from_bytes(packet.load[:1], byteorder='big'))  # Message type
                                 ptp_info.append(float(packet.time - initial_time))
                                 packet_queue.put(ptp_info)
-                                print(f'Adding {ptp_info} to buffer')
+                                print(f'Adding {ptp_info} to queue')
                                 initial_time = packet.time
                         packet_buffer = b""  # Clear buffer after successful processing
                     except Scapy_Exception:
+                        print(f"Scapy Exception {Scapy_Exception}")
                         pass  # Wait for more data if packets are incomplete
         except Exception as e:
             print(f"Error reading from file: {str(e)}")
